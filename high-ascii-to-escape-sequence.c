92d6974cec1adfade340d3aace0a2549a660008f
1500100900 <2134918@gmail.com>
2013-04-02 21:47:46 +0300

    Create high-ascii-to-escape-sequence.c

---
+++ high-ascii-to-escape-sequence.c
@@ -0,0 +1,114 @@
+/* The effect of learning FSMs from Kate. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static FILE *in, *out;
+
+static void options(int argc, char *argv[]) {
+  if (argc == 1) {
+		in = stdin;
+		out = stdout;
+		return;
+	}
+ 
+	if (argc != 3) {
+		fprintf(stderr, "Expected either 0 or 2 arguments.\n");
+		exit(EXIT_FAILURE);
+	}
+	if (!(in = fopen(argv[1], "r"))) {
+		perror(argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	if (!(out = fopen(argv[2], "w"))) {
+		perror(argv[2]);
+		exit(EXIT_FAILURE);
+	}
+ 
+	return;
+}
+
+static inline void unable_to_write(void) {
+	perror("Unable to write to destination file");
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+	options(argc, argv);
+
+	typedef enum token {
+		N, NEWLINE = N,
+		D, DOUBLE = D,
+		S, SLASH = S,
+		H, HIGH = H,
+		A, ASTERISK = A,
+		C, CHAR = C,
+		MAX_TOKEN
+	}
+	token;
+
+	enum effect {
+		PUT,
+		ESC
+	};
+
+	enum state {
+		CODE, /* code */
+		STRN, /* string */
+		MBCM, /* maybe comment */
+		CCOM, /* C comment */
+		MBCD, /* maybe code */
+		CPPC, /* C++ comment */
+		MAX_STATE
+	};
+
+	typedef struct transition {
+		unsigned char next, effect;
+	}
+	transition;
+
+	transition table[MAX_STATE][MAX_TOKEN] = {
+		/*                NEWLINE,         DOUBLE,          SLASH,           HIGH,       ASTERISK,           CHAR */
+		[CODE] = { [N]={CODE,PUT}, [D]={STRN,PUT}, [S]={MBCM,PUT}, [H]={CODE,ESC}, [A]={CODE,PUT}, [C]={CODE,PUT} },
+		[STRN] = { [N]={STRN,PUT}, [D]={CODE,PUT}, [S]={STRN,PUT}, [H]={STRN,ESC}, [A]={STRN,PUT}, [C]={STRN,PUT} },
+		[MBCM] = { [N]={CODE,PUT}, [D]={CODE,PUT}, [S]={CPPC,PUT}, [H]={CODE,PUT}, [A]={CCOM,PUT}, [C]={CODE,PUT} },
+		[CCOM] = { [N]={CCOM,PUT}, [D]={CCOM,PUT}, [S]={CCOM,PUT}, [H]={CCOM,PUT}, [A]={MBCD,PUT}, [C]={CCOM,PUT} },
+		[MBCD] = { [N]={CCOM,PUT}, [D]={CCOM,PUT}, [S]={CODE,PUT}, [H]={CCOM,PUT}, [A]={CCOM,PUT}, [C]={CCOM,PUT} },
+		[CPPC] = { [N]={CODE,PUT}, [D]={CPPC,PUT}, [S]={CPPC,PUT}, [H]={CPPC,PUT}, [A]={CPPC,PUT}, [C]={CPPC,PUT} }
+	};
+
+	int c;
+	transition state = {CODE, CHAR};
+
+	while (c = getc(in), c != EOF) {
+		token t;
+
+		switch (c) {
+		case '*':  t = ASTERISK; break;
+		case '"':  t = DOUBLE;   break;
+		case '/':  t = SLASH;    break;
+		case '\n': t = NEWLINE;  break;
+		default:   t = (c & 128) ? HIGH : CHAR; break;
+		}
+
+		state = table[state.next][t];
+
+		switch (state.effect) {
+		case PUT:
+			if (EOF == putc(c, out))
+				unable_to_write();
+			break;
+		case ESC:
+			if (4 > fprintf(out, "\\%o", (unsigned char) c))
+				unable_to_write();
+			break;
+		}
+	}
+
+	if (ferror(in)) {
+		perror("Unable to read from source file");
+		exit(EXIT_FAILURE);
+	}
+ 
+	exit(EXIT_SUCCESS);
+}
